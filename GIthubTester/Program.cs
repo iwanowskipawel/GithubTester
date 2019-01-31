751154a797f821a9bd8dd144353a2d2319e1d380
Paweł Iwanowski <iwanowski3.14@gmail.com>
2019-01-31 14:21:03 +0100

    Add project files.

---
+++ GIthubTester/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIthubTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //testing github
+            //Upload repository from local folder on PC to github
+        }
+    }
+}
